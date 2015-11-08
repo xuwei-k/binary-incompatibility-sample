b4d52be56bdb003b7661e1e8b9d5ed561ee608c4
xuwei-k <6b656e6a69@gmail.com>
2015-11-08 20:36:04 +0900

    abstract class

--- library/foldableNew/Foldable.scala
+++ library/foldableNew/Foldable.scala
@@ -1,6 +1,6 @@
 package com.example
 
-trait Foldable[F[_]]{
+abstract class Foldable[F[_]]{
   def foldLeft[A, B](fa: F[A], z: B)(f: (B, A) => B): B
   def toList[A](fa: F[A]): List[A] =
     foldLeft(fa, List.empty[A])((xs, x) => x :: xs).reverse
